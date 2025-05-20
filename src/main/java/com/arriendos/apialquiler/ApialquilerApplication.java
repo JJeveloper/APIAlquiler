ede9e926f66342ec9f9ae6e24e0435344919b904
Jarood <jaroodjimili@gmail.com>
2025-05-20 12:22:22 -0500

    iniciando proyecto

---
+++ src/main/java/com/arriendos/apialquiler/ApialquilerApplication.java
@@ -0,0 +1,13 @@
+package com.arriendos.apialquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApialquilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApialquilerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arriendos/apialquiler/ApialquilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arriendos.apialquiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApialquilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
